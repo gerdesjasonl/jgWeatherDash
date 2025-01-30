62df9197e07c57b600fd26951d73e8b16ca289df
gerdesjasonl <gerdesjasonl@gmail.com>
2025-01-30 08:35:43 -0500

    Updated started code from repo

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,13 +1,65 @@
+import dayjs, { type Dayjs } from 'dayjs';
 import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
 
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: Dayjs | string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  icon: string;
+  iconDescription: string;
+  constructor(
+    city: string,
+    date: Dayjs | string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+    icon: string,
+    iconDescription: string
+  ) {
+    this.city = city;
+    this.date = date;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
+  
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  private city = '';
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
+  // * Note: The following methods are here as a guide, but you are welcome to provide your own solution.
+  // * Just keep in mind the getWeatherForCity method is being called in your
+  // * 09-Servers-and-APIs/02-Challenge/Develop/server/src/routes/api/weatherRoutes.ts file
+  
+  // * the array of Weather objects you are returning ultimately goes to
+  // * 09-Servers-and-APIs/02-Challenge/Develop/client/src/main.ts
+
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
   // TODO: Create destructureLocationData method
